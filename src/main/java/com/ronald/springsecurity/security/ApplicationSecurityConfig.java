34e590d3b362bea7da82facd968d089fdc1f69e0
Kiet Nguyen <6393445-19520666NK@users.noreply.gitlab.com>
2022-07-26 14:53:31 +0700

    Add ant matcher

--- src/main/java/com/ronald/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/ronald/springsecurity/security/ApplicationSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,9 +19,13 @@ public class ApplicationSecurityConfig  {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
+                        .antMatchers("/", "index", "/css/*", "/js/*")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
                 )
                 .httpBasic(withDefaults());
         return http.build();
     }
+
 }

1415b6956a80be5a9404f7c896e98e4c907ebc44
Kiet Nguyen <6393445-19520666NK@users.noreply.gitlab.com>
2022-07-27 16:53:22 +0700

    Permission based authority su dung anotation PreAuthorize trong controller

--- src/main/java/com/ronald/springsecurity/student/StudentManagementController.java
+++ src/main/java/com/ronald/springsecurity/student/StudentManagementController.java
@@ -2,6 +2,7 @@ package com.ronald.springsecurity.student;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -20,24 +21,28 @@ public class StudentManagementController {
     private final Logger LOGGER = LoggerFactory.getLogger(StudentManagementController.class);
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         LOGGER.info("Inside get all students");
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         LOGGER.info("Inside register new student");
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
         LOGGER.info("Inside delete student");
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
         LOGGER.info(String.format("Inside put student %s %s%n", studentId, student));
     }
